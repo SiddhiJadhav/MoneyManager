5e9d8c2f18f2c507ae68156f1bb30bc1e901f557
SiddhiJadhav <sidjadhav989@gmail.com>
2024-12-25 10:06:57 +0530

    Reading URL from env file

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-
-export const supabaseUrl = 'https://watlpbuzhxfmewrwmyuq.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndhdGxwYnV6aHhmbWV3cndteXVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ4MDY0MDMsImV4cCI6MjA1MDM4MjQwM30.hbO7kC3qcy94RdIka2zqItZviuRY41C_gbFMt8fc2Vc';
+debugger;
+export const supabaseUrl = import.meta.env.VITE_URL;
+const supabaseKey = import.meta.env.VITE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
