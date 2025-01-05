7d3d59dfc3882bdeba682081077bb51f53bc4f77
SiddhiJadhav <sidjadhav989@gmail.com>
2025-01-05 21:22:48 +0530

    added netlify.toml file

--- src/features/authentication/AuthProvider.jsx
+++ src/features/authentication/AuthProvider.jsx
@@ -6,31 +6,38 @@ export const AuthContext = createContext();
 export default function AuthProvider({ children }) {
   const [user, setUser] = useState();
   const [session, setSession] = useState();
-  const [isloading, setIsLoading] = useState();
-
+  const [isLoading, setIsLoading] = useState(false);
+  debugger;
   useEffect(() => {
     async function setData() {
-      const { data: session } = await supabase.auth.getSession();
-      console.log(session);
+      //setIsLoading(true);
+      //const { data: session } = await supabase.auth.getSession();
+      // console.log(session);
 
-      if (!session) return null;
+      //if (!session.session) return null;
 
-      setSession(session);
-      setUser(session?.session?.user);
-      setIsLoading(false);
+      //setSession(session);
+
+      const { isLoading, data: user } = await supabase.auth.getUser();
+
+      if (!user) return null;
+      debugger;
+      setUser(user);
+      setIsLoading(isLoading);
+      console.log(user);
     }
     setData();
   }, []);
 
   const values = {
     user,
-    isloading,
+    isLoading,
   };
   console.log(values);
 
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 }
 
-export const useAuth = () => {
-  return useContext(AuthContext);
-};
+// export const useAuth = () => {
+//   return useContext(AuthContext);
+// };
--- src/features/authentication/useAuth.js
+++ src/features/authentication/useAuth.js
@@ -1,5 +1,8 @@
-import { createContext, useContext } from 'react';
-const AuthContext = createContext();
+import { useContext } from 'react';
+import { AuthContext } from './AuthProvider';
+//const AuthContext = createContext();
 export function useAuth() {
   const { user, isLoading } = useContext(AuthContext);
+
+  return { isLoading, user, isAuthenticated: user?.role === 'authenticated' };
 }
