b0e58aeb03a784120590609bcbc06c995af54657
SiddhiJadhav <sidjadhav989@gmail.com>
2025-01-05 22:35:15 +0530

    Resolved the login issue by invalidating query

--- src/features/authentication/useLoginUser.js
+++ src/features/authentication/useLoginUser.js
@@ -4,15 +4,15 @@ import { loginUser as loginUserApi } from '../../services/apiAuth';
 import { useNavigate } from 'react-router-dom';
 
 export function useLoginUSer() {
-  //const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const { mutate: loginUser, isLoading: isLogging } = useMutation({
     mutationFn: loginUserApi,
     onSuccess: () => {
       toast.success('Login Successful');
+      queryClient.invalidateQueries({ queryKey: ['user'] });
       navigate('/dashboard', { replace: true });
-      //queryClient.invalidateQueries({ queryKey: ['finance'] });
     },
     onError: err => toast.error(err.message),
   });
